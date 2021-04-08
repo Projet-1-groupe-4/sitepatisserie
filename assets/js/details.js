83b5959a85e5c8e5eb74997d632771809484636c
Delino <64547069+d3lino@users.noreply.github.com>
2021-04-08 11:04:51 +0100

    création des modals

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,3 +1,13 @@
-function showInfo() {
-    alert("ALERT");
-}
+var mod1 = document.getElementById("gateau1");
+var btn1 = document.getElementById("btn1");
+var span = document.getElementsByClassName("close")[0];
+
+btn1.onclick = mod1.style.display = "block";
+span.onclick = mod1.style.display = "none";
+
+
+/*window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}*/
